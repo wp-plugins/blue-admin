3e93c08aeb945f3ba54d54cb31eb81590509e3f8
lineshjose <lineshjose@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2012-08-09 18:49:55 +0000

    git-svn-id: https://plugins.svn.wordpress.org/blue-admin/tags/12.06@583633 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- index.php
+++ index.php
@@ -8,61 +8,65 @@ Author: Linesh Jose
 Author URI: http://lineshjose.com
 License: GPL2
 */
-	function blue_admin()
+	$version='12.06';
+	
+	// Login page validation //
+	if(!function_exists('lj_is_login_page'))
 	{
-		wp_register_style( 'blue-admin', WP_PLUGIN_URL . '/blue-admin/style.css', false, '12.06.01' );
-		wp_enqueue_style( 'blue-admin' );
+		function lj_is_login_page() {
+		return in_array($GLOBALS['pagenow'], array('wp-login.php', 'wp-register.php'));
+		}
 	}
+	
+	// Function to set default css style//
+	if(!function_exists('lj_admin_styles'))	
+	{
+		function lj_admin_styles()
+		{
+			global $wp_lj_plugin;
+			if(is_admin() || lj_is_login_page()){
+				wp_register_style( 'blue_admin', plugin_dir_url(__FILE__) . 'style.css','',$version );
+			}
+			if(is_user_logged_in()){
+				wp_register_style( 'blue_admin_admin_bar',  plugin_dir_url(__FILE__). 'admin-bar.css','', $version );
+			}
+			 wp_enqueue_style('blue_admin');
+			 wp_enqueue_style('blue_admin_admin_bar');
+		}
+		add_action('admin_enqueue_scripts', 'lj_admin_styles');
+		add_action('login_head', 'lj_admin_styles');
+		add_action('wp_head', 'lj_admin_styles');
+	}
+	
+	
+	
+	
+	
+	
+	// Function to display credit text in footer//
 	function footer_credit()
 	{
 		echo '<span id="footer-thankyou">Thank you for creating with <a href="http://wordpress.org/">WordPress</a>.</span>
 			<span id="footer-thankyou">This theme developed by <a href="http://lineshjose.com/">Linesh Jose</a>.</span>';
 	}
-	function custom_login()
-	{
-		echo '
-			<style type="text/css">
-								
-				.login{font-size:11px!important; font-family: "lucida grande",tahoma,verdana,arial,sans-serif !important;color: #333;line-height: 1.28;direction: ltr;unicode-bidi: embed; margin:0!important; padding:0 !important; background:#FFF!important; }
-				
-				.login #nav a, .login #backtoblog a,.login #nav a:hover, .login #backtoblog a:hover {color: #3B5998!important;font-size:11px!important; }
-				
-				.login #login{padding: 10% 0 0; margin:auto!important; height:auto; width:350px}	
-				.login h1 a {background-position: center center;	width: 350px;}		
-				.login form	 {background:#FBFBFB; text-align: left;position: relative;border: 0px solid #AAA;	box-shadow: 0px 0px 00px #fff;border-radius: 0px!important;padding: 26px 24px 36px;}
-				.login form .input{border: 1px solid #BDC7D8;font-family: "lucida grande",tahoma,verdana,arial,sans-serif;font-size: 11px;margin: 0 0 15px;padding: 3px;-webkit-appearance: none;-webkit-border-radius: 0;-moz-border-radius:0!important;border-radius:0!important;}
-				.login form .button-primary{padding: 3px 16px!important; line-height:15px!important; font-size:11px!important;background: #617AAC url(\''.WP_PLUGIN_URL . '/blueadmin/images/button.png\') repeat-x 100% -47px !important;border: 1px solid #29447E!important;border: 1px solid #999;-webkit-box-shadow: 0 1px 0 rgba(0, 0, 0, .1); -webkit-border-radius:0;-moz-border-radius:0!important;border-radius:0!important; color:#EAF2FA!important;}
-				.login form .button-primary:active{background:#617AAC!important}
-				.login form .button-primary:hover{color:#fff!important;}
-				.login form label, .login form .forgetmenot label{font-size:11px;}
-				
-				#nav{float:right;}
-				#nav,#backtoblog {padding: 0!important;margin:5px!important;}
-				</style>';
-	}
-			
-	if (is_admin()) 
-	{
-		add_action('admin_enqueue_scripts', 'blue_admin');
-		add_filter('admin_footer_text', 'footer_credit'); 
-	}
-	add_action('login_head', 'custom_login');
+	add_filter('admin_footer_text', 'footer_credit'); 
+	
+		
 
 
 	// Add meta links
-	function blueadmin_actions( $links, $file ) {
-	$plugin = plugin_basename(__FILE__);
- 
-	// create link
-	if ($file == $plugin) {
-		//$links[] = '<a href="' . admin_url( 'options-general.php?page=blueadmin' ) . '">' . __('Settings', TPTN_LOCAL_NAME ) . '</a>';
-		$links[] = '<a href="http://bit.ly/go_fund">' . __('Donate', TPTN_LOCAL_NAME ) . '</a>';
+	function blueadmin_actions( $links, $file )
+	{
+		$plugin = plugin_basename(__FILE__);
+		if ($file == $plugin) {
+			$links[] = '<a href="http://bit.ly/donate-blue-admin">' . __('Donate', TPTN_LOCAL_NAME ) . '</a>';
+			$links[] = '<a href="http://tech.lineshjose.com/introducing-blue-admin/#comments" target="_blank">' . __('Support', TPTN_LOCAL_NAME ) . '</a>';
+		}
+		return $links;
 	}
-	return $links;
-	}
+	
 	global $wp_version;
-	if ( version_compare( $wp_version, '3.2', '>' ) )
-	add_filter( 'plugin_row_meta', 'blueadmin_actions', 10, 2 ); // only 2.8 and higher
-	else add_filter( 'plugin_action_links', 'blueadmin_actions', 10, 2 );
+	if ( version_compare( $wp_version, '3.2', '>' ) ){add_filter( 'plugin_row_meta', 'blueadmin_actions', 10, 2 ); } // only 2.8 and higher
+	else {add_filter( 'plugin_action_links', 'blueadmin_actions', 10, 2 );}
 
 ?>
--- readme.txt
+++ readme.txt
@@ -1,56 +1,74 @@
-=== Plugin Name ===
+=== Plugin Name === 
 Contributors: lineshjose
-Plugin Name: Blue Admin
-Plugin URI: http://tech.lineshjose.com/introducing-blue-admin/
-Tags: Blue Admin,clearadmin,clear,admin,theme,admin theme,administration,wordpress administration,facebook style,facebook looks-like,blue,blue admin bar,floating admin menu
-Author URI: http://lineshjose.com/
-Author: Linesh Jose
-Donate link: http://bit.ly/go_fund
-Requires at least: 3.2
-Tested up to: 3.3.2
+Author: Linesh Jose  
+Author URI: http://lineshjose.com/ 
+Plugin Name: Blue Admin 
 Stable tag: 12.06
 Version: 12.06
-License: GPLv2 or later
+Requires at least: 3.2 
+Tested up to: 3.4.1 
+License: GPLv2 or later 
+Plugin URI: http://tech.lineshjose.com/introducing-blue-admin/ 
+Tags: Blue Admin,clearadmin,clear,admin,theme,admin theme,administration,WordPress administration,facebook style,facebook looks-like,blue,blue admin bar,custom menus,custom navigation,adminbar navigation,adminbar menus 
+Donate link: http://bit.ly/donate-blue-admin 
 
-This is a simple and clear admin design that makes your WordPress administration section more clear and relaxed.
+ 
+This is a simple and clear admin design that makes your WordPress administration section more clear and relaxed. 
+ 
+ 
+== Description == 
+ 
+This is a simple and clear admin design that makes your **WordPress** administration section more clear and relaxed. Read more about **[Blue Admin](http://tech.lineshjose.com/introducing-blue-admin/)**. 
+ 
+**Features** 
+ 
+* Simple, Clean and clear admin design. 
+  
+Watch this video below, to know how to enable/install Blue Admin Plugin on your WordPress site. 
+[youtube http://www.youtube.com/watch?v=H3W4R23QlFk] 
+ 
+ 
+ 
+ 
+== Installation == 
+ 
+1. Upload Blue Admin folder to the `/wp-content/plugins/` directory or download it from the administration 
+2. Activate the plugin through the 'Plugins' menu in WordPress 
+3. Navigate in WordPress Admin Pages and see the new, clean design! 
+ 
+Watch this video below, to know how to enable/install **Blue Admin** Plugin on your WordPress site. 
+[youtube http://www.youtube.com/watch?v=H3W4R23QlFk] 
+ 
 
-== Description ==
+ 
+== Frequently Asked Questions == 
+ 
+= Does the plugin change WordPress layout? = 
+Yes, this plugin will make these changes on your administration section. 
+ 
+1. It changes the CSS Style admin section, thickbox and login form. 
+2. It changes the themes listing style. 
+4. It changes the icons 
+ 
+= Is the plugin compatible with all WP versions? = 
+ 
+No, because WordPress could change after each update the admin style, and the plugin is based on it. So if you want to be sure to see the correct style of Blue Admin you need last WordPress version. 
+ 
+ 
+== Screenshots ==  
+ 
+1. Screenshot
+ 
+== Changelog == 
 
-This is a simple and clear admin design that makes your WordPress administration section more clear and relaxed.
-
-== Installation ==
-
-1. Upload Blue Admin folder to the `/wp-content/plugins/` directory or download it from the administration
-2. Activate the plugin through the 'Plugins' menu in WordPress
-3. Navigate in Wordpress Admin Pages and see the new, clean design!
-
-== Frequently Asked Questions ==
-
-= Does the plugin change Wordpress layout? =
-Yes, this plugin will make these changes on your administration section.
-
-1. It changes the CSS Style admin section, thickbox and login form.
-2. It changes the themes listing style.
-4. It changes the icons
-
-= Is the plugin compatible with all WP versions? =
-
-No, because Wordpress could change after each update the admin style, and the plugin is based on it. So if you want to be sure to see the correct style of Blue Admin you need last Wordpress version.
-
-== Screenshots ==
-
-1. screenshot-1.jpg
-
-
-== Changelog ==
-
-= 12.06 =
-Fixed Admin menu scrolling issue
-
-= 12.05 (First version.) =
-
-The Blue Admin version numbering scheme is based on the release  date. The version number comes from the year and month of the release rather than reflecting the actual version of the plugin.
-
-== Support ==
-For support & more informations visit http://lineshjose.com/contact
-To contact the author the mail is lineshjose(@)gmail(.)com 
+= 12.06 = 
+Fixed Admin menu scrolling issue. Changed client side admin bar style and Some other CSS modifications.  
+ 
+= 12.05  = 
+First version. 
+ 
+*Our version numbering scheme is based on the release  date. The version number comes from the year and month  of the release.* 
+ 
+== Support == 
+For support & more information visit http://lineshjose.com/contact 
+To contact the author the mail is lineshjose(@)gmail(.)com   
